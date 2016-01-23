483fed251fa1be5896074a0db916ddde84b9453c
SuperKillerMonkE <superkillermonke@gmail.com>
2016-01-23 09:33:09 -0500

    Update etherealcode.py

--- etherealcode.py
+++ etherealcode.py
@@ -1,1 +1,5 @@
-print('Hello, world!')
+yn1 = raw_input('The end is coming...\nAre you prepared?')
+if yn1 == yes or Yes: print("Then you are prepared. Welcome, to the dreamworld.")
+while yn1 != yes or Yes: yn1 = raw_input("Then I will wait until you are ready. Are you prepared?")
+if yn1 == yes or Yes: 
+    break
